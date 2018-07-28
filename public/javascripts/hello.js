1e01aadecca0d862a1b1705382442a603b9dded2
rmitcmsproject <41687051+rmitcmsproject@users.noreply.github.com>
2018-07-28 16:00:10 +1000

    login page and eclipse

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,11 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
+  $('.toggle').on('click', function() {
+  	  $('.container').stop().addClass('active');
+  	});
+
+  	$('.close').on('click', function() {
+  	  $('.container').stop().removeClass('active');
+  	});
 }
+
