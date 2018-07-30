c1563fb476586adcf10815d136c6d26d4546be34
rmitcmsproject <41687051+rmitcmsproject@users.noreply.github.com>
2018-07-30 14:00:53 +1000

    Add files via upload

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render());
+        return ok(index.render("CMS",main.render()));
     }
 
 }
