55c185609b52040d995adc41ed0d1cb6ca8c6c48
rmitcmsproject <41687051+rmitcmsproject@users.noreply.github.com>
2018-08-20 15:37:07 +1000

    role and error

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,17 +1,39 @@
 app.controller('loginController', function($scope) {
-   
+
 	if (window.console) {
-		  console.log("Welcome to your Play application's JavaScript!");
-		  $('.toggle').on('click', function() {
-		  	  $('.container').stop().addClass('active');
-		  	});
+		console.log("Welcome to your Play application's JavaScript!");
+		$('.toggle').on('click', function() {
+			$('.mycontainer').stop().addClass('active');
+		});
 
-		  	$('.close').on('click', function() {
-		  	  $('.container').stop().removeClass('active');
-		  	});
+		$('.close').on('click', function() {
+			$('.mycontainer').stop().removeClass('active');
+		});
+	}
+
+	$scope.user = {};
+	$scope.title = "LOGIN";
+
+	$scope.register = function() {
+
+		if ($scope.user.role && $scope.user.ID && $scope.user.pass
+				&& $scope.user.passConfirm) {
+			if ($scope.user.pass == $scope.user.passConfirm)
+				console.log($scope.user);
+			else
+				$scope.errorMessage("Error","Password Dont Match");
+				
+
 		}
+	}
 
-$scope.user={};
-
+	
+	$scope.errorMessage = function(mytype,mymessage){
+		$scope.$parent.modal={};
+		$scope.$parent.modal.type=mytype;
+		$scope.$parent.modal.message=mymessage;
+		$("#myModal").modal();
+		
+	}
 	
 });
